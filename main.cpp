20a96d4cb4be2eb52a730d2f99add76192552055
freyt <jcsardap1998@gmail.com>
2020-11-04 16:05:48 -0500

    Improve bilinear implementation

--- main.cpp
+++ main.cpp
@@ -16,25 +16,28 @@ using namespace std;
 const int INPUT_SIZE = 256;
 
 /*
-    Nearest Neighbor is the simplest and worst upscaling algorithm.
+    Nearest Neighbor is the simplest upscaling algorithm.
     Creates a 2x2 matrix for every individual pixel in the original image.
 
     This implementation is O(n), where n is the number of pixels in the 
-    original image.
+    original image (INPUT_SIZE^2).
 
     @param in the input image.
     @param out the output image.
 */
 void nearest_neighbor(int in[][INPUT_SIZE], int out[][INPUT_SIZE*2]) {
-    for (int i = 0; i < INPUT_SIZE * 2; i++) {
-        for (int j = 0; j < INPUT_SIZE * 2; j++) {
-            out[i][j] = in[i/2][j/2];
+    for (int i = 0; i < INPUT_SIZE; i++) {
+        for (int j = 0; j < INPUT_SIZE; j++) {
+            out[i][j] = in[i][j];
+            out[i+1][j] = in[i][j];
+            out[i][j+1] = in[i][j];
+            out[i+1][j+1] = in[i][j];
         }
     }
 }
 
 /*
-    Bilinear interpolation.
+    Bilinear upscaling.
     Uses linear interpolation to fill in missing pixels in
     output image.
 
@@ -46,43 +49,85 @@ void nearest_neighbor(int in[][INPUT_SIZE], int out[][INPUT_SIZE*2]) {
     Every 2x2 matrix in the original image is turned into a
     4x4 matrix on the output image.
 
+    This implementation is O(n), where n is the number of pixels in the 
+    original image (INPUT_SIZE^2).
+
     @param in the input image.
     @param out the output image.
 */
 void bilinear(int in[][INPUT_SIZE], int out[][INPUT_SIZE*2]) {
+    // perform linear interpolation in every known row
     for (int i = 0; i < INPUT_SIZE / 2; i++) {
         for (int j = 0; j < INPUT_SIZE / 2; j++) {
-            // fill in known pixels a, b, c, d
+            for (int k = 0; k < 4; k++) {
+                out[i*4][j*4+k] = in[i*2][j*2] + (in[i*2][j*2+1] - in[i*2][j*2]) * k / 3;
+                out[i*4+3][j*4+k] = in[i*2+1][j*2] + (in[i*2+1][j*2+1] - in[i*2+1][j*2]) * k / 3;
+            }
+        }
+    }
 
-            out[i*4][j*4] = in[i*2][j*2]; // a
-            out[i*4+3][j*4] = in[i*2+1][j*2]; // b
-            out[i*4][j*4+3] = in[i*2][j*2+1]; // c
-            out[i*4+3][j*4+3] = in[i*2+1][j*2+1]; // d
+    // perform linear interpolation in every column
+    for (int j = 0; j < INPUT_SIZE * 2; j++) {
+        for (int i = 0; i < INPUT_SIZE / 2; i++) {
+            for (int k = 0; k < 4; k++) {
+                out[i*4+k][j] = out[i*4][j] + (out[i*4+3][j] - out[i*4][j]) * k / 3;
+            }
+        }
+    }
+}
 
-            // horizontal interpolation to calculate pixels between a-b and c-d
-            out[i*4+1][j*4] = in[i*2][j*2] + (in[i*2+1][j*2] - in[i*2][j*2]) / 3;
-            out[i*4+2][j*4] = in[i*2][j*2] + (in[i*2+1][j*2] - in[i*2][j*2]) / 3 * 2;
-            out[i*4+1][j*4+3] = in[i*2][j*2+1] + (in[i*2+1][j*2+1] - in[i*2][j*2+1]) / 3;
-            out[i*4+2][j*4+3] = in[i*2][j*2+1] + (in[i*2+1][j*2+1] - in[i*2][j*2+1]) / 3 * 2;
+/*
+    Bicubic upscaling.
+    Uses cubic interpolation to fill in missing pixels in
+    output image.
 
-            // vertical interpolation to calculate pixels between a-c and b-d
-            out[i*4][j*4+1] = in[i*2][j*2] + (in[i*2][j*2+1] - in[i*2][j*2]) / 3;
-            out[i*4][j*4+2] = in[i*2][j*2] + (in[i*2][j*2+1] - in[i*2][j*2]) / 3 * 2;
-            out[i*4+3][j*4+1] = in[i*2+1][j*2] + (in[i*2+1][j*2+1] - in[i*2+1][j*2]) / 3;
-            out[i*4+3][j*4+2] = in[i*2+1][j*2] + (in[i*2+1][j*2+1] - in[i*2+1][j*2]) / 3 * 2;
+        a b c d ->   a _ _ b _ _ c _ _ d
+        e f g h      _ _ _ _ _ _ _ _ _ _
+        i j k l      _ _ _ _ _ _ _ _ _ _
+        m n o p      e _ _ f _ _ g _ _ h
+                     _ _ _ _ _ _ _ _ _ _
+                     _ _ _ _ _ _ _ _ _ _
+                     i _ _ j _ _ k _ _ l
+                     _ _ _ _ _ _ _ _ _ _
+                     _ _ _ _ _ _ _ _ _ _
+                     m _ _ n _ _ o _ _ p
 
-            // horizontal interpolation to calculate remaining pixels
-            out[i*4+1][j*4+1] = out[i*4][j*4+1] + (out[i*4+3][j*4+1] - out[i*4][j*4+1]) / 3;
-            out[i*4+2][j*4+1] = out[i*4][j*4+1] + (out[i*4+3][j*4+1] - out[i*4][j*4+1]) / 3 * 2;
-            out[i*4+1][j*4+2] = out[i*4][j*4+2] + (out[i*4+3][j*4+2] - out[i*4][j*4+2]) / 3;
-            out[i*4+2][j*4+2] = out[i*4][j*4+2] + (out[i*4+3][j*4+2] - out[i*4][j*4+2]) / 3 * 2;
+    Every 4x4 matrix in the original image is turned into a
+    10x10 matrix on the output image.
+
+    This implementation is O(n), where n is the number of pixels in the 
+    original image (INPUT_SIZE^2).
+
+    @param in the input image.
+    @param out the output image.
+*/
+void bicubic(int in[][INPUT_SIZE], int out[][INPUT_SIZE*2]) {
+    for (int i = 0; i < INPUT_SIZE / 4; i++) {
+        for (int j = 0; j < INPUT_SIZE / 4; j++) {
+            // fill in known pixels a-p
+            out[i*10][j*10] = in[i*4][j*4];
+            out[i*10+3][j*10] = in[i*4+1][j*4];
+            out[i*10+6][j*10] = in[i*4+2][j*4];
+            out[i*10+9][j*10] = in[i*4+3][j*4];
+            out[i*10][j*10+3] = in[i*4][j*4+1];
+            out[i*10+3][j*10+3] = in[i*4+1][j*4+1];
+            out[i*10+6][j*10+3] = in[i*4+2][j*4+1];
+            out[i*10+9][j*10+3] = in[i*4+3][j*4+1];
+            out[i*10][j*10+6] = in[i*4][j*4+2];
+            out[i*10+3][j*10+6] = in[i*4+1][j*4+2];
+            out[i*10+6][j*10+6] = in[i*4+2][j*4+2];
+            out[i*10+9][j*10+6] = in[i*4+3][j*4+2];
+            out[i*10][j*10+9] = in[i*4][j*4+3];
+            out[i*10+3][j*10+9] = in[i*4+1][j*4+3];
+            out[i*10+6][j*10+9] = in[i*4+2][j*4+3];
+            out[i*10+9][j*10+9] = in[i*4+3][j*4+3];
         }
     }
 }
 
 /*
     Reads image information from a byte array from given file.
-    The image is expected to be of size INPUT_SIZE x INPUT_SIZE.
+    The image is expected to be of size INPUT_SIZE^2.
 
     Returns true if image was successfully read, and false otherwise.
 
